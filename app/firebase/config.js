8b65837d60cf71e51186dd2ed749199b2cdcb671
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-31 14:38:17 +0100

    Create initial project files and set up firebase

---
+++ app/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGNvNQTVDQU3gtKZCP-XRLKPEtjET_2yM",
+  authDomain: "buzz-books.firebaseapp.com",
+  projectId: "buzz-books",
+  storageBucket: "buzz-books.appspot.com",
+  messagingSenderId: "117389728648",
+  appId: "1:117389728648:web:b91f0eb7c6d680473ef83d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
