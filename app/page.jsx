000971b11871238e2160f704c44e194c7f7033f9
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-08-01 09:25:35 +0100

    start work on home and navbar components

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,8 @@ import LandingPage from "./components/LandingPage";
 export default function Home() {
   return (
     <main>
-      <LandingPage/>
+      
+      {/* <LandingPage/> */}
     </main>
   )
 }
