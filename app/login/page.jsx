5a34466a8ecd44ecd20a575d41e232aec0317a4e
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-01 12:06:09 +0100

    Added error handling to the form. Added basic CSS styling.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,10 +2,13 @@
 import { useState } from "react";
 import app from "../firebase/config";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const auth = getAuth(app);
 
 const LoginPage = () => {
+    let router = useRouter();
+    const [isError, setIsError] = useState(null);
     const [loginData, setLoginData] = useState({
         email: "",
         password: "",
@@ -21,42 +24,52 @@ const LoginPage = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
+        setIsError(null);
         try {
             const response = await signInWithEmailAndPassword(auth, loginData.email, loginData.password);
-
             console.log(response);
+            router.push("/");
         } catch (error) {
-            console.error("Error logging in: ", error);
+            setIsError(error.message);
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                value={loginData.email}
-                placeholder="Enter a valid email..."
-                onChange={handleChange}
-                required
-            />
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <h2>Please log in below.</h2>
 
-            <label htmlFor="password">Password</label>
-            <input
-                type="password"
-                id="password"
-                name="password"
-                value={loginData.password}
-                placeholder="Enter a valid password..."
-                onChange={handleChange}
-                required
-            />
+                <div className="input-wrapper">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        value={loginData.email}
+                        placeholder="Enter a valid email..."
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
 
-            <button className="bg-blue-500 p-2 rounded text-white">Log In</button>
-        </form>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={loginData.password}
+                        placeholder="Enter a valid password..."
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <button className="bg-blue-500 p-2 rounded text-white">Log In</button>
+
+                {isError ? <p>{isError}</p> : null}
+            </form>
+        </div>
     );
 };
 
