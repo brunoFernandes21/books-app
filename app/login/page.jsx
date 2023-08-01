ea6ad6aeb8887225a12af40ce605c6cd4c189218
Anna Tasheva <anna.tasheva90@gmail.com>
2023-08-01 10:44:48 +0100

    Created a login form. Implemented login functionality with email and password.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,63 @@
+"use client";
+import { useState } from "react";
+import app from "../firebase/config";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth(app);
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+    });
 
-export default LoginPage
+    const handleChange = (event) => {
+        setLoginData((previousData) => {
+            const { name, value } = event.target;
+
+            return { ...previousData, [name]: value };
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await signInWithEmailAndPassword(auth, loginData.email, loginData.password);
+
+            console.log(response);
+        } catch (error) {
+            console.error("Error logging in: ", error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+                type="text"
+                id="email"
+                name="email"
+                value={loginData.email}
+                placeholder="Enter a valid email..."
+                onChange={handleChange}
+                required
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                value={loginData.password}
+                placeholder="Enter a valid password..."
+                onChange={handleChange}
+                required
+            />
+
+            <button className="bg-blue-500 p-2 rounded text-white">Log In</button>
+        </form>
+    );
+};
+
+export default LoginPage;
