099564c8bcf9526fe9b2f232c01321407904aa71
1991hannah <122830800+1991hannah@users.noreply.github.com>
2023-08-03 10:29:36 +0100

    Updating context

--- app/context/User.jsx
+++ app/context/User.jsx
@@ -4,6 +4,8 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+    const [user, setUser] = useState("");
     return <UserContext.Provider value={{ user, setUser }}>{props.children}</UserContext.Provider>;
 };
+
+// JSON.parse(localStorage.getItem("user"))
