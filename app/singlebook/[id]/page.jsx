16118c132a3dfeaef2bf9d9a68e3e6bd547f50f6
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-08-09 10:18:11 +0100

    add try and catch and add notification so user knows if they successfully added a book or if the book is already saved

--- app/singlebook/[id]/page.jsx
+++ app/singlebook/[id]/page.jsx
@@ -2,7 +2,13 @@
 import { useEffect, useState, useContext } from "react";
 import React from "react";
 import Link from "next/link";
-import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "@/app/firebase/config";
 import { useParams } from "next/navigation";
 import { fetchBookById } from "@/app/api/route";
@@ -15,6 +21,9 @@ function SingleBookPage() {
   const [singleBook, setSingleBook] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [buttonError, setButtonError] = useState(null);
+  const [dupplicateMessage, setDupplicateMessage] = useState(null);
+  const [bookAdded, setBookAdded] = useState(null);
   const { id } = useParams();
   let router = useRouter();
   const { user, setUser } = useContext(AuthContext);
@@ -47,30 +56,99 @@ function SingleBookPage() {
 
   const addToFavourites = async () => {
     const docRef = doc(db, "userData", user.uid);
-    const updateAction = await updateDoc(docRef, {
-      favourites: arrayUnion({ bookID: id, bookInfo: singleBook }),
-    });
+
+    try {
+      setButtonError(null);
+      const docSnap = await getDoc(docRef);
+      const userData = docSnap.data();
+      const isDupplicate = userData.favourites.some(
+        (favouriteBook) => favouriteBook.bookID === id
+      );
+      if (!isDupplicate) {
+        setDupplicateMessage(null);
+        const updateAction = await updateDoc(docRef, {
+          favourites: arrayUnion({ bookID: id, bookInfo: singleBook }),
+        });
+        setBookAdded("Book successfully added to your Favourites");
+      } else {
+        setDupplicateMessage("Book already in your Favourites");
+      }
+    } catch (error) {
+      setButtonError("Unable to add to Favourites");
+      setDupplicateMessage(null);
+    }
   };
 
   const addToCurrentlyReading = async () => {
     const docRef = doc(db, "userData", user.uid);
-    const updateAction = await updateDoc(docRef, {
-      currentlyReading: arrayUnion({ bookID: id, bookInfo: singleBook }),
-    });
+    try {
+      setButtonError(null);
+      const docSnap = await getDoc(docRef);
+      const userData = docSnap.data();
+      const isDupplicate = userData.currentlyReading.some(
+        (currentlyReadingBook) => currentlyReadingBook.bookID === id
+      );
+      if (!isDupplicate) {
+        setDupplicateMessage(null);
+        const updateAction = await updateDoc(docRef, {
+          currentlyReading: arrayUnion({ bookID: id, bookInfo: singleBook }),
+        });
+        setBookAdded("Book successfully added to your Currently Reading");
+      } else {
+        setDupplicateMessage("Book already in your Currently Reading");
+      }
+    } catch (error) {
+      setButtonError("Unable to add to Currently Reading");
+      setDupplicateMessage(null);
+    }
   };
 
   const saveForLater = async () => {
     const docRef = doc(db, "userData", user.uid);
-    const updateAction = await updateDoc(docRef, {
-      savedBooks: arrayUnion({ bookID: id, bookInfo: singleBook }),
-    });
+    try {
+      setButtonError(null);
+      const docSnap = await getDoc(docRef);
+      const userData = docSnap.data();
+      const isDupplicate = userData.savedBooks.some(
+        (savedBook) => savedBook.bookID === id
+      );
+      if (!isDupplicate) {
+        setDupplicateMessage(null);
+        const updateAction = await updateDoc(docRef, {
+          savedBooks: arrayUnion({ bookID: id, bookInfo: singleBook }),
+        });
+        setBookAdded("Book successfully added to your Save For Later");
+      } else {
+        setDupplicateMessage("Book already in your Save For Later");
+      }
+    } catch (error) {
+      setButtonError("Unable to add to Save For Later");
+      setDupplicateMessage(null);
+    }
   };
 
   const markAsRead = async () => {
     const docRef = doc(db, "userData", user.uid);
-    const updateAction = await updateDoc(docRef, {
-      readBooks: arrayUnion({ bookID: id, bookInfo: singleBook }),
-    });
+    try {
+      setButtonError(null);
+      const docSnap = await getDoc(docRef);
+      const userData = docSnap.data();
+      const isDupplicate = userData.readBooks.some(
+        (readBook) => readBook.bookID === id
+      );
+      if (!isDupplicate) {
+        setDupplicateMessage(null);
+        const updateAction = await updateDoc(docRef, {
+          readBooks: arrayUnion({ bookID: id, bookInfo: singleBook }),
+        });
+        setBookAdded("Book successfully added to your Mark As Read");
+      } else {
+        setDupplicateMessage("Book already in your Mark As Read");
+      }
+    } catch (error) {
+      setButtonError("Unable to add to Mark As Read");
+      setDupplicateMessage(null);
+    }
   };
 
   const clearHTMLTags = (strToSanitize) => {
@@ -83,8 +161,12 @@ function SingleBookPage() {
 
   return (
     <main>
-      {error && <p className="text-2xl font-bold text-center mt-96">No books found</p>}
-      {loading && !error && <p className="text-2xl font-bold text-center mt-96">Loading...</p>}
+      {error && (
+        <p className="text-2xl font-bold text-center mt-96">No books found</p>
+      )}
+      {loading && !error && (
+        <p className="text-2xl font-bold text-center mt-96">Loading...</p>
+      )}
       {!loading && (
         <article className="mt-10 flex justify-center items-center m-auto bg-white rounded-2xl">
           <div className="m-auto p-4 ">
@@ -109,6 +191,21 @@ function SingleBookPage() {
                 Published {singleBook.publishedDate}
               </p>
               <p className="mt-2">{clearHTMLTags(singleBook.description)}</p>
+              {buttonError !== null && (
+                <div className='bg-red-100 border my-4 border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" text-center'>
+                  <span className="font-bold">{buttonError}</span>
+                </div>
+              )}
+              {dupplicateMessage !== null && (
+                <div className='bg-red-100 border my-4 border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" text-center'>
+                  <span className="font-bold">{dupplicateMessage}</span>
+                </div>
+              )}
+              {dupplicateMessage === null && bookAdded !== null && (
+                <div className='bg-green-100 border my-4 border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert" text-center'>
+                  <span className="font-bold">{bookAdded}</span>
+                </div>
+              )}
               <div className="w-4/5 m-auto p-4">
                 <button
                   className="mb-2 w-full md:w-1/2 bg-blue-800 hover:bg-blue-400 font-black text-center py-3 rounded-full text-white my-1"
@@ -141,7 +238,6 @@ function SingleBookPage() {
       )}
     </main>
   );
-
 }
 
 export default SingleBookPage;
