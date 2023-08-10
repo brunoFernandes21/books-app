abd81fa44205f51c9e35bba34524d44bf54b324b
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-08-10 15:54:21 +0100

    added back in search criteria

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -28,23 +28,23 @@ const Search = ({ setBooks, books , setError, setShowSearchResults}) => {
 
   const inputRef = useRef();
 
-  // useEffect(() => {    
-  //   setSearchCriteria("intitle")
+  useEffect(() => {    
+    setSearchCriteria("intitle")
 
-  //   }, [])
+    }, [])
 
-  // const setSearchCriteria = (criteria) => {
-  //   setCriteria(criteria)
-  //   const currentButton = document.getElementById(criteria)
-  //   currentButton.classList.add("button-focus")
-  //   const buttons = document.querySelectorAll('button');
-  //   const arrayList = Array.from(buttons)
-  //   const removedButtons = arrayList.filter((button) => 
-  //   {return button.id !== criteria})
-  //   removedButtons.forEach((button) => {
-  //     button.classList.remove("button-focus")
-  //   })
-  // }
+  const setSearchCriteria = (criteria) => {
+    setCriteria(criteria)
+    const currentButton = document.getElementById(criteria)
+    currentButton.classList.add("button-focus")
+    const buttons = document.querySelectorAll('button');
+    const arrayList = Array.from(buttons)
+    const removedButtons = arrayList.filter((button) => 
+    {return button.id !== criteria})
+    removedButtons.forEach((button) => {
+      button.classList.remove("button-focus")
+    })
+  }
 
   const bookScanner = () => {
     function onScanSuccess(decodedText, decodedResult) {
