5b94e963d866f9e7873f61969ecfc4f21b381fed
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-08-10 10:27:46 +0100

    added crtieria focus on page load

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getBooksBySearchTerm } from "../api/route";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import {BsCamera} from "react-icons/bs"
@@ -25,6 +25,26 @@ const Search = ({ setBooks, books , setError, setShowSearchResults}) => {
     }
   };
 
+  const inputRef = useRef();
+
+  useEffect(() => {    
+    setSearchCriteria("intitle")
+
+    }, [])
+
+  const setSearchCriteria = (criteria) => {
+    setCriteria(criteria)
+    const currentButton = document.getElementById(criteria)
+    currentButton.classList.add("button-focus")
+    const buttons = document.querySelectorAll('button');
+    const arrayList = Array.from(buttons)
+    const removedButtons = arrayList.filter((button) => 
+    {return button.id !== criteria})
+    removedButtons.forEach((button) => {
+      button.classList.remove("button-focus")
+    })
+  }
+
   const bookScanner = () => {
     function onScanSuccess(decodedText, decodedResult) {
       if (decodedText.length === 10 || decodedText.length === 13) {
@@ -47,8 +67,13 @@ const Search = ({ setBooks, books , setError, setShowSearchResults}) => {
       },
       fps: 10,
     })
+
+
     return scanner.render(onScanSuccess, onScanFailure);
   }
+
+  // console.log("ref", inputRef)
+
 
   return (
     <div className="p-4 m-auto mt-5 flex justify-center items-center flex-col bg-white rounded-2xl text-slate-800 max-w-2xl">
@@ -56,10 +81,10 @@ const Search = ({ setBooks, books , setError, setShowSearchResults}) => {
 
       <div className="flex flex-row gap-4 mt-4 justify-center items-center">
         <p>Search by :</p>
-        <div className="flex gap-4 text-white">
-          <button autoFocus id="title-button"className="bg-slate-800 p-2 rounded-full text-sm font-bold focus:bg-white focus:text-slate-800 focus:ring focus:ring-violet-800" onClick={() => setCriteria("intitle")}>Title</button>
-          <button className="bg-slate-800 p-2 rounded-full text-sm font-bold focus:bg-white focus:text-slate-800 focus:ring focus:ring-violet-800" onClick={() => setCriteria("inauthor")}>Author</button>
-          <button className="bg-slate-800 p-2 rounded-full text-sm font-bold focus:bg-white focus:text-slate-800 focus:ring focus:ring-violet-800" onClick={() => setCriteria("ISBN")}>ISBN</button>
+        <div className="flex gap-4 text-white" ref={inputRef}>
+          <button autoFocus className="bg-slate-800 p-2 rounded-full text-sm font-bold" onClick={() => setSearchCriteria("intitle")} id="intitle" >Title</button>
+          <button className="bg-slate-800 p-2 rounded-full text-sm font-bold" onClick={() => setSearchCriteria("inauthor")} id="inauthor">Author</button>
+          <button className="bg-slate-800 p-2 rounded-full text-sm font-bold" onClick={() => setSearchCriteria("ISBN")} id="ISBN">ISBN</button>
         </div>
       </div>
       <form onSubmit={handleSubmit} className=" flex justify-center items-center mt-4 w-full">
